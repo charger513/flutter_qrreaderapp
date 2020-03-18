7256a49788f4fd9bb7c128f532f79e46dbb5f705
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-18 16:57:32 -0600

    SQFLite - CREATE Table y Database

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DBProvider {
@@ -15,6 +19,23 @@ class DBProvider {
   }
 
   initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
 
+    final path = join(documentsDirectory.path, 'ScansDB.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          'CREATE TABLE Scans ('
+          ' id INTEGER PRIMARY KEY,'
+          ' tipo TEXT,'
+          ' valor TEXT'
+          ')'
+        );
+      }
+    );
   }
 }
