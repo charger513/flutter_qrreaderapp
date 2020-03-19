a97feb4dca53951d656fe18444bc304e64332cad
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-19 11:48:08 -0600

    Página de direcciones

--- lib/src/pages/mapas_page.dart
+++ lib/src/pages/mapas_page.dart
@@ -33,7 +33,7 @@ class MapasPage extends StatelessWidget {
             ),
             onDismissed: (direction) => scansBloc.borrarScan(scans[i].id),
             child: ListTile(
-              leading: Icon(Icons.cloud_queue, color: Theme.of(context).primaryColor),
+              leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
               title: Text(scans[i].valor),
               subtitle: Text('ID: ${scans[i].id}'),
               trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
