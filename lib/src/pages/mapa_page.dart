0726273e639a2eef47dc3687e201b76b82b63ff5
Emmanuel Chargoy <charger513@hotmail.com>
2020-03-18 19:37:22 -0600

    Crear un mapa en Flutter

--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:latlong/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_qrreaderapp/src/models/scan_model.dart';
 
 class MapaPage extends StatelessWidget {
@@ -17,7 +19,30 @@ class MapaPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(child: Text(scan.valor),)
+      body: _crearFlutterMap(scan)
+    );
+  }
+
+  Widget _crearFlutterMap(ScanModel scan) {
+    return FlutterMap(
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 5.0
+      ),
+      layers: [
+        _crearMapa(),
+      ],
+    );
+  }
+
+  _crearMapa() {
+    return TileLayerOptions(
+      urlTemplate: 'https://api.mapbox.com/v4/'
+      '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+      additionalOptions: {
+        'accessToken': 'pk.eyJ1IjoiY2hhcmdlcjUxMyIsImEiOiJjazd5MXk1YzQwMnBiM2xxbGRoY3V2MXRhIn0.P8FmU9KcY1D_-sMyT8f6kw',
+        'id': 'mapbox.streets'
+      }
     );
   }
 }
